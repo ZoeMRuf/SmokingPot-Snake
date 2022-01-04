50f4c47a52036646751f8b07b6bf0e4e42b923d1
konstant_coding <konstantin.papirnik@gmail.com>
2022-01-04 18:19:50 +0100

    Erste Schritte GameEnd.java

--- src/main/java/at/ac/fhcampuswien/GameEnd.java
+++ src/main/java/at/ac/fhcampuswien/GameEnd.java
@@ -1,8 +1,25 @@
 package at.ac.fhcampuswien;
 
-public class GameEnd {
+// Erklärung: Ich würde bei jeder Bewegung des Heads der Schlange testen ob sich die Schlange auf ein safes Feld bewegt
+// Das setted dann eine Flag safe auf 0 oder 1, dann prüft GameEnd den Zustand dieser Flag und endet oder continued eben
+// Kompiliert noch nicht, weil es erst ein Objekt Snake mit der safe Flag braucht (ohne Objekt und nur mit Klasse
+// verlangt es dann dass safe static gemacht wird, aber wollen ja dann vom konkreten Objekt prüfen)
+public class GameEnd(Snake.safe) {
+    if (Snake.safe == 0) {
+        System.out.println("Test");
+
+    }
+
+
+
+
     /*
     1) GameWon
     2) GameOver
      */
+
+
+
+
+
 }
