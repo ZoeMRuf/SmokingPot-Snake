7c334bdec8189a8620b3c957d1fa023c244a533d
Simon786 <56161871+Simon786@users.noreply.github.com>
2022-01-10 15:18:11 +0100

    Update Food.java

--- src/main/java/at/ac/fhcampuswien/Food.java
+++ src/main/java/at/ac/fhcampuswien/Food.java
@@ -1,8 +1,26 @@
 package at.ac.fhcampuswien;
 
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import java.util.Random;
 
 public class Food {
 
+    private Rectangle food;
 
+    Random r = new Random();
+
+    public Food() {
+        food.setFill(Color.rgb(255,0,150));
+        food.setHeight(App.snake.gridSize-5);
+        food.setWidth(App.snake.gridSize-5);
+
+    }
+
+    public void setRandomFood(){
+        food.setX(r.nextInt(App.snake.scale)*App.snake.gridSize+App.snake.gridSize);
+        food.setY(r.nextInt(App.snake.scale)*App.snake.gridSize+App.snake.gridSize);
+        App.root.getChildren().addAll(food);
+    }
 }
