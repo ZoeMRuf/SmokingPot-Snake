86f7326719c246e5e68f544f9bdf8a914746156e
Simon786 <56161871+Simon786@users.noreply.github.com>
2022-01-12 15:32:16 +0100

    Update App.java

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -53,13 +53,17 @@ public class App extends Application {
 
         snake = new Snake();
         gameLoop = new GameLoop();
+        gameLoop.timeLine.stop();
 
         //Menu scene:
         Label label = new Label("Welcome to our Version of Snake");
         Button startGame = new Button("Start the Game");
         Button showHighScore = new Button("Show High-score");
 
-        startGame.setOnAction(e -> primaryStage.setScene(sceneGame));
+         startGame.setOnAction(event -> {
+            gameLoop.timeLine.play();
+            primaryStage.setScene(sceneGame);
+        });
         Button gameOverButton = new Button("Test GameOver");
         gameOverButton.setOnAction(e -> primaryStage.setScene(sceneGameOver));
 
@@ -69,7 +73,10 @@ public class App extends Application {
 
         //GameOver Scene:
         Button playAgain = new Button("Play again");
-        playAgain.setOnAction(e -> primaryStage.setScene(sceneGame));
+        playAgain.setOnAction(e -> {
+            gameLoop.timeLine.play();
+            primaryStage.setScene(sceneGame);
+        });
         Button backToMenu = new Button("Back to Menu");
         backToMenu.setOnAction(e -> primaryStage.setScene(sceneMenu));
 
