cc6bf59e9b97e62a784c48753909c460735943d1
konstant_coding <konstantin.papirnik@gmail.com>
2022-01-14 17:58:34 +0100

    Added Test for win condition in GameLoop.java

--- src/main/java/at/ac/fhcampuswien/GameLoop.java
+++ src/main/java/at/ac/fhcampuswien/GameLoop.java
@@ -33,6 +33,7 @@ public class GameLoop {
             getInput();
             snakeMoved = true;
             GameOver.snakeLeavesGameBoard();
+            Win.GameWin();
             if(!GameOver.isGameOver) {
                 App.snake.moveSnake();
                 App.snake.doesSnakeEat();
