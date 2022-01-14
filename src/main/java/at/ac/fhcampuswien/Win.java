b36bfe4dfb0cc1e033351c7a40a671d39b684dc5
konstant_coding <konstantin.papirnik@gmail.com>
2022-01-14 17:07:46 +0100

    Small update for Win.java

--- src/main/java/at/ac/fhcampuswien/Win.java
+++ src/main/java/at/ac/fhcampuswien/Win.java
@@ -2,10 +2,7 @@ package at.ac.fhcampuswien;
 
 public class Win {
 
-    int outOfGame = App.GameSize;
-
     public static boolean isGameWin = false;
-
     public static boolean GameWin() {
 
         if (App.score == 324-1) {
